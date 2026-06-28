e8e37c6671553b325a366dc04a6fa0faafc5b416
Zack Seuberling <seuberling@gmail.com>
2026-06-28 11:43:14 -0700

    First commit

---
+++ pages/api/index.js
@@ -0,0 +1,26 @@
+const { groq } = require("next-sanity");
+const { getClient } = require("../../lib/sanity");
+
+export default async function handler(request, response) {
+  try {
+    const { featuredProductions } = await getClient()
+      .fetch(groq`*[_type == "homepage"][0]{
+      featuredProductions[]-> {
+        _type,
+        _id,
+        slug,
+        country,
+        dates,
+        title,
+        featuredImage,
+        eventType[]->,
+      }
+    }`);
+
+    console.log(featuredProductions);
+
+    return response.status(200).json({ body: ["results"] });
+  } catch (error) {
+    return response.status(500).json({ body: JSON.stringify(error) });
+  }
+}
